1f9244c7c1d13c857d09379b45ccfece0abba8f2
flak <flak@flakhost.flakdomain>
2017-05-03 09:23:45 -0400

    print

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask
 import requests, json, time
 from flask_cors import CORS, cross_origin
 import mraa
+import atexit
+from apscheduler.scheduler import Scheduler
 
 url = "https://api.darksky.net/forecast/d19642947c22791bf57113f02837e88f/40.9257,-73.1409"
 
@@ -9,6 +11,27 @@ url = "https://api.darksky.net/forecast/d19642947c22791bf57113f02837e88f/40.9257
 
 app = Flask(__name__)
 CORS(app)
+cron = Scheduler(daemon=True)
+cron.start()
+
+@cron.interval_schedule(hours=6)
+def rain_check():
+    duration = 1.0
+    if measure_moisture() < 1:
+        duration+=1
+    if measure_moisture() < 2:
+        duration+=.5
+    water(duration)
+
+    payload = {
+        'exclude': 'flags,minutely,hourly',
+    }
+    response = requests.get(url, params=payload)
+    response = response.json()
+    print(response)
+
+
+atexit.register(lambda: cron.shutdown(wait=False))
 
 @app.route('/current')
 def current_weather():
@@ -45,16 +68,23 @@ def history(days_back):
 
 
 @app.route('/water/<float:valve_time>')
+def water_endpoint(valve_time):
+    water(valve_time)
+    return 'watered'
+
 def water(valve_time):
     valve = mraa.Gpio(12)
     valve.dir(mraa.DIR_OUT)
     valve.write(1)
     time.sleep(valve_time)
     valve.write(0)
-    return 'watered'
 
 @app.route('/moisture')
 def current_moisture():
+    return str(float(measure_moisture()))
+
+
+def measure_moisture():
     sensor7 = mraa.Gpio(7)
     sensor7.dir(mraa.DIR_IN)
     print(sensor7.read())
@@ -70,5 +100,4 @@ def current_moisture():
     sensor4 = mraa.Gpio(4)
     sensor4.dir(mraa.DIR_IN)
     print(sensor4.read())
-    return str(float(sensor7.read() + sensor6.read() + sensor5.read() + sensor4.read()))
-
+    return float(sensor7.read() + sensor6.read() + sensor5.read() + sensor4.read())
