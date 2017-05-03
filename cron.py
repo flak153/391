bc2c449752c5f93a406d73df8da530af8a99c25b
flak <flak@flakhost.flakdomain>
2017-05-03 09:50:30 -0400

    more precise

--- cron.py
+++ cron.py
@@ -38,7 +38,7 @@ def rain_check():
     }
     response = requests.get(url, params=payload)
     response = response.json()
-    print(response['currently']['precipIntensity'])
+    print(response['currently'])
     if measure_moisture() > 3:
         duration*=0
     water(duration)
